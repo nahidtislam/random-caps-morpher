d07e06abd18dbd274a8b6f820a0fcf64898c0416
Nahid <75819596+nahidtislam@users.noreply.github.com>
2023-10-23 22:30:10 +0100

    Initial Commit

---
+++ random caps morpher/random_caps_morpherApp.swift
@@ -0,0 +1,17 @@
+//
+//  random_caps_morpherApp.swift
+//  random caps morpher
+//
+//  Created by Nahid Islam on 23/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct random_caps_morpherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
