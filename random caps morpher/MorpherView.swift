a3828000097d9557e20a2e64d75515ff0383fdc8
Nahid <75819596+nahidtislam@users.noreply.github.com>
2023-10-24 21:42:20 +0100

    the view ani,atescwell

--- random caps morpher/MorpherView.swift
+++ random caps morpher/MorpherView.swift
@@ -15,17 +15,24 @@ struct MorpherView: View {
     
     var body: some View {
         VStack {
-            Button("clear text", role: .destructive) {
-                withAnimation(controlTransitionAnimation) {
-                    vm.userText = ""
-                }
+            Button(role: .destructive) {
+                vm.userText = ""
+            } label: {
+                Label("clear", systemImage: vm.morphedTextIsEmpty ? "clear.fill" : "clear")
             }
             .disabled(vm.userText.isEmpty)
             .frame(maxWidth: .infinity, alignment: .topTrailing)
             
-            TextEditor(text: $vm.userText.animation())
+            TextEditor(text: $vm.userText)
+                .padding()
+                .background {
+                    Color.textBackgroundColor
+                }
                 .clipShape(RoundedRectangle(cornerRadius: 18, style: .continuous))
-            if vm.userText.isEmpty {
+                #if os(macOS)
+                .frame(minWidth: 800, minHeight: 450)
+                #endif
+            if vm.morphedTextIsEmpty {
                 directionToUser
             } else {
                 VStack {
@@ -35,6 +42,7 @@ struct MorpherView: View {
                 .transition(.move(edge: .bottom).combined(with: .opacity).combined(with: .scale(scale: 0.85, anchor: .bottom)).animation(controlTransitionAnimation))
             }
         }
+        .animation(.spring(response: 0.2, dampingFraction: 2, blendDuration: 0.1), value: vm.morphedTextIsEmpty)
         .padding()
         .background {
             bg
@@ -59,8 +67,8 @@ struct MorpherView: View {
         let dark: Color
         let light: Color
         
-        dark = .init(uiColor: .secondarySystemBackground)
-        light = .init(uiColor: .systemGray6)
+        dark = .secondarySystemBackground
+        light = .systemGray6
         
         return colorScheme == .dark ? dark : light
     }
@@ -71,10 +79,11 @@ struct MorpherView: View {
                 .onAppear {
                     vm.upperCaseness = 4
                 }
-            Button("copy") {
-                UIPasteboard.general.setValue(vm.morphed, forPasteboardType: "public.plain-text")
+            Button(action: vm.copyToClipboard) {
+                Label("copy", systemImage: "arrow.right.doc.on.clipboard")
             }
             .buttonStyle(.bordered)
+            .keyboardShortcut(KeyEquivalent("c"), modifiers: .command)
             .disabled(vm.morphed.count < 4)
         }
     }
@@ -84,7 +93,8 @@ struct MorpherView: View {
             .padding()
             .frame(maxWidth: .infinity, alignment: .trailing)
             .background {
-                Color(uiColor: .systemBackground).opacity(colorScheme == .dark ? 0.2 : 0.6)
+                
+                Color.systemBackground.opacity(colorScheme == .dark ? 0.2 : 0.6)
             }
             .clipShape(RoundedRectangle(cornerRadius: 22, style: .continuous))
     }
@@ -96,4 +106,37 @@ struct MorpherView: View {
 
 #Preview {
     MorpherView()
+}
+
+extension Color {
+    static var systemBackground: Color {
+        #if os(macOS)
+        .init(nsColor: .controlBackgroundColor)
+        #else
+        .init(uiColor: .systemBackground)
+        #endif
+    }
+    
+    static var secondarySystemBackground: Color {
+        #if os(macOS)
+        .init(nsColor: NSColor.scrubberTexturedBackground)
+        #else
+        .init(uiColor: .secondarySystemBackground)
+        #endif
+    }
+    static var systemGray6: Color {
+        #if os(macOS)
+        .init(#colorLiteral(red: 0.8948548286, green: 0.8948548286, blue: 0.8948548286, alpha: 1))
+        #else
+        .init(uiColor: .systemGray6)
+        #endif
+    }
+    
+    static var textBackgroundColor: Color {
+        #if os(macOS)
+        Color(nsColor: .textBackgroundColor)
+        #else
+        Color(uiColor: .systemBackground)
+        #endif
+    }
 }
