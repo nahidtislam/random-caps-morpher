ece502034c55fda1762c77fbfe8cabc26f45d99d
Nahid <75819596+nahidtislam@users.noreply.github.com>
2023-10-24 08:03:00 +0100

    added animations and seprated the view components

--- random caps morpher/MorpherView.swift
+++ random caps morpher/MorpherView.swift
@@ -16,36 +16,34 @@ struct MorpherView: View {
     var body: some View {
         VStack {
             Button("clear text", role: .destructive) {
-                vm.userText = ""
+                withAnimation(controlTransitionAnimation) {
+                    vm.userText = ""
+                }
             }
             .disabled(vm.userText.isEmpty)
             .frame(maxWidth: .infinity, alignment: .topTrailing)
             
-            TextEditor(text: $vm.userText)
+            TextEditor(text: $vm.userText.animation())
                 .clipShape(RoundedRectangle(cornerRadius: 18, style: .continuous))
-            HStack {
-                Slider(value: sliderValue, in: 3...7, step: 1.0)
-                    .onAppear {
-                        vm.upperCaseness = 4
-                    }
-                Button("copy") {
-                    UIPasteboard.general.setValue(vm.morphed, forPasteboardType: "public.plain-text")
+            if vm.userText.isEmpty {
+                directionToUser
+            } else {
+                VStack {
+                    controls
+                    output
                 }
-                .buttonStyle(.bordered)
+                .transition(.move(edge: .bottom).combined(with: .opacity).combined(with: .scale(scale: 0.85, anchor: .bottom)).animation(controlTransitionAnimation))
             }
-            Text(vm.morphed)
-                .padding()
-                .frame(maxWidth: .infinity)
-                .background(Material.thin)
-                .clipShape(RoundedRectangle(cornerRadius: 22, style: .continuous))
-            
-            .disabled(vm.morphed.count < 4)
         }
         .padding()
         .background {
             bg
-                .ignoresSafeArea()
+            .ignoresSafeArea()
         }
+    }
+    
+    private var controlTransitionAnimation: Animation {
+        .spring(response: 0.3, dampingFraction: 1.5, blendDuration: 0.2).speed(1.5)
     }
     
     private var sliderValue: Binding<Double> {
@@ -65,6 +63,34 @@ struct MorpherView: View {
         light = .init(uiColor: .systemGray6)
         
         return colorScheme == .dark ? dark : light
+    }
+    
+    private var controls: some View {
+        HStack {
+            Slider(value: sliderValue, in: 3...7, step: 1.0)
+                .onAppear {
+                    vm.upperCaseness = 4
+                }
+            Button("copy") {
+                UIPasteboard.general.setValue(vm.morphed, forPasteboardType: "public.plain-text")
+            }
+            .buttonStyle(.bordered)
+            .disabled(vm.morphed.count < 4)
+        }
+    }
+    
+    private var output: some View {
+        Text(vm.morphed)
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .trailing)
+            .background {
+                Color(uiColor: .systemBackground).opacity(colorScheme == .dark ? 0.2 : 0.6)
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 22, style: .continuous))
+    }
+    
+    private var directionToUser: some View {
+        Text("enter text above")
     }
 }
 
