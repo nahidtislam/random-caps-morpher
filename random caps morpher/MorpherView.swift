ebf16b8da0c066af60cba020bbee6e16ac73f77f
Nahid <75819596+nahidtislam@users.noreply.github.com>
2023-10-24 07:44:11 +0100

    made view look preety

--- random caps morpher/MorpherView.swift
+++ random caps morpher/MorpherView.swift
@@ -11,6 +11,8 @@ struct MorpherView: View {
     
     @StateObject var vm: ViewModel = .init()
     
+    @Environment(\.colorScheme) var colorScheme
+    
     var body: some View {
         VStack {
             Button("clear text", role: .destructive) {
@@ -18,29 +20,51 @@ struct MorpherView: View {
             }
             .disabled(vm.userText.isEmpty)
             .frame(maxWidth: .infinity, alignment: .topTrailing)
+            
             TextEditor(text: $vm.userText)
-                .lineLimit(12)
-            Slider(value: sliderValue, in: 3...7, step: 1.0)
-                .onAppear {
-                    vm.upperCaseness = 4
+                .clipShape(RoundedRectangle(cornerRadius: 18, style: .continuous))
+            HStack {
+                Slider(value: sliderValue, in: 3...7, step: 1.0)
+                    .onAppear {
+                        vm.upperCaseness = 4
+                    }
+                Button("copy") {
+                    UIPasteboard.general.setValue(vm.morphed, forPasteboardType: "public.plain-text")
                 }
-            Text(vm.morphed)
-            Button("copy") {
-                UIPasteboard.general.setValue(vm.morphed, forPasteboardType: "public.plain-text")
+                .buttonStyle(.bordered)
             }
-            .buttonStyle(.bordered)
+            Text(vm.morphed)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Material.thin)
+                .clipShape(RoundedRectangle(cornerRadius: 22, style: .continuous))
+            
             .disabled(vm.morphed.count < 4)
         }
         .padding()
+        .background {
+            bg
+                .ignoresSafeArea()
+        }
     }
     
-    var sliderValue: Binding<Double> {
+    private var sliderValue: Binding<Double> {
         .init {
             .init(vm.upperCaseness)
         } set: { newValue in
             vm.upperCaseness = .init(newValue)
         }
 
+    }
+    
+    private var bg: Color {
+        let dark: Color
+        let light: Color
+        
+        dark = .init(uiColor: .secondarySystemBackground)
+        light = .init(uiColor: .systemGray6)
+        
+        return colorScheme == .dark ? dark : light
     }
 }
 
