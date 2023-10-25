e5948f7e1f1506e0942577e2921253c5fbc03e86
Nahid <75819596+nahidtislam@users.noreply.github.com>
2023-10-25 08:08:58 +0100

    removed default value on the class itself and updated the subscriber code

--- random caps morpher/MorpherView+ViewModel.swift
+++ random caps morpher/MorpherView+ViewModel.swift
@@ -11,41 +11,55 @@ import Combine
 extension MorpherView {
     class ViewModel: ObservableObject {
         
-        @Published var userText: String = .init()
-        @Published var morphed: String = .init()
-        @Published var upperCaseness: Int = .init()
-        @Published var shouldShowMorped: Bool = false
+        @Published var userText: String
+        @Published var morphed: String
+        @Published var upperCaseness: Int
+        
+        @Published var morphedTextIsEmpty: Bool
+        // should be used in a notification sense
+        @Published var shouldShowMorped: Bool
         
         private var cancelables: Set<AnyCancellable> = .init()
         
         convenience init() {
-            self.init(userText: .init(), morphed: .init(), upperCaseness: 5, shouldShowMorped: false, cancelables: .init())
+            self.init(userText: .init(), morphed: .init(), upperCaseness: 5, shouldShowMorped: false)
         }
         
-        init(userText: String, morphed: String, upperCaseness: Int, shouldShowMorped: Bool, cancelables: Set<AnyCancellable>) {
+        init(userText: String, morphed: String, upperCaseness: Int, morphedTextIsEmpty: Bool? = nil, shouldShowMorped: Bool) {
             self.userText = userText
             self.morphed = morphed
             self.upperCaseness = upperCaseness
+            
+            self.morphedTextIsEmpty = morphedTextIsEmpty ?? morphed.isEmpty
             self.shouldShowMorped = shouldShowMorped
-            self.cancelables = cancelables
+            
+            self.cancelables = .init()
             
             setupSubscribers()
         }
         
-        func setupSubscribers() {
+        private func setupSubscribers() {
             $userText
                 .debounce(for: .milliseconds(75), scheduler: DispatchQueue.main)
                 .combineLatest($upperCaseness)
                 .debounce(for: .milliseconds(25), scheduler: DispatchQueue.main)
                 .map(performMorph)
-                .assign(to: &$morphed)
+                .sink { [weak self] morphed in
+                    guard let self else { return }
+                    self.morphed = morphed
+                    self.morphedTextIsEmpty = morphed.isEmpty
+                    self.shouldShowMorped = morphed.count > 3
+                }
+                .store(in: &cancelables)
+//                .assign(to: &$morphed)
                 
         }
         
         private func performMorph(on text: String, upCaseness: Int) -> String {
             var output: String = .init()
             
-            text.forEach { character in
+            // TODO: figure out how to map the string's character
+            text.trimmingCharacters(in: .whitespacesAndNewlines).forEach { character in
                 output += Int.random(in: 0..<10) < upCaseness ? character.uppercased() : character.lowercased()
             }
             
